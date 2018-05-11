0e8cc9d70778ba3eafdba4801e59c7a114414803
Ashwani Gupta <ashwanigupta125@gmail.com>
2018-05-11 20:29:25 +0530

    Refactor responses

--- server/api/controllers/question.js
+++ server/api/controllers/question.js
@@ -4,19 +4,20 @@ exports.get = (req, res) => {
   const limit = Number(req.query.limit);
 
   if (!limit) {
-    res.status(404).json({
+    res.status(400).json({
       error: { message: "limit is not provided" }
     });
+    return;
   }
 
   let options = { limit: limit };
   Question.findRandom({}, {}, options, (err, quotes) => {
-    if (!err) {
-      res.send(quotes);
-    } else {
+    if (err) {
       res.status(500).json({
-        error: { message: "No results found" }
+        error: { message: "Something went wrong" }
       });
+      throw err;
     }
+    res.send(quotes);
   });
 };
